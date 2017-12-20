5f16508182fb5fbb4ae1d6fe548b0f57b9075787
pankaj101Y <pnkcomm2u@gmail.com>
2017-12-20 19:41:05 +0530

    server urls change

--- app/src/main/java/com/example/ks/mobileverificationsystem/ServerContacts.java
+++ app/src/main/java/com/example/ks/mobileverificationsystem/ServerContacts.java
@@ -3,10 +3,10 @@ package com.example.ks.mobileverificationsystem;
 class ServerContacts {
 
     static String getSendOTPURL(){
-        return "https://fast-sea-53512.herokuapp.com/sendOTP";
+        return "< server url which sends OTP Users Phone >";
     }
 
     static String getVerifyOTPURL(){
-        return "https://fast-sea-53512.herokuapp.com/verifyOTP";
+        return "server url which verifies OTP  received from Users Phone ";
     }
 }
